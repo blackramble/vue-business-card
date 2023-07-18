79279876da93a1910dbb92e24e395d28c24e649f
blackramble <bjtfetopa@gmail.com>
2023-07-19 00:16:51 +0800

    feat: 🎸 更多初始設定

---
+++ src/components/MainContainer/MainContainer.vue
@@ -0,0 +1,15 @@
+<script setup>
+console.log('MainContainer');
+</script>
+
+<template>
+    <div class="main-container">MainContainer</div>
+</template>
+
+<style scoped lang="scss">
+.main-container {
+    max-width: 680px;
+
+    // background-color: aqua;
+}
+</style>
