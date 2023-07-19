67b0096b39ea86efd4fcce044822c80d0ffd1672
blackramble <bjtfetopa@gmail.com>
2023-07-20 00:32:01 +0800

    feat: 🎸 vercel/analytics

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import MainContainer from './components/MainContainer/MainContainer.vue';
+import { inject } from '@vercel/analytics';
+
+inject();
 </script>
 
 <template>
